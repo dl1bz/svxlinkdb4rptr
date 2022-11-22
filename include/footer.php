79e1a8245f0a38562baff13461c86c299728b8d0
dl1bz <dg2dra@gmx.net>
2022-11-22 16:40:31 +0100

    change footer

--- include/footer.php
+++ include/footer.php
@@ -1,2 +1,2 @@
 <!--- Please do not remove copyright info -->
-<center><span title="Dashboard" style="font: 7pt arial, sans-serif;">SvxLink Dashboard ©  SP2ONG, SP0DZ <?php $cdate=date("Y"); if ($cdate > "2021") {$cdate="2021-".date("Y");} echo $cdate; ?> | rewrite and modified by DL1BZ for public repeaters © <?php echo date("Y"); ?>
+<center><span title="Dashboard" style="font: 7pt arial, sans-serif;">based at <A HREF="https://github.com/FM-POLAND/hs_dashboard_pi">SvxLink Dashboard</A> by SP2ONG & SP0DZ | rewrite by DL1BZ for public repeaters © <?php echo date("Y")." | served by ".$_SERVER['SERVER_SOFTWARE']." & PHP ".phpversion(); ?>
