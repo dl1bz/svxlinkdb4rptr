c139fff2c27a7a74c4418750685b3a7187ce0f85
dl1bz <dg2dra@gmx.net>
2022-11-22 18:53:58 +0100

    change footer

--- include/footer.php
+++ include/footer.php
@@ -1,2 +1,2 @@
 <!--- Please do not remove copyright info -->
-<center><span title="Dashboard" style="font: 7pt arial, sans-serif;">based at <A HREF="https://github.com/FM-POLAND/hs_dashboard_pi">SvxLink Dashboard</A> by SP2ONG & SP0DZ | rewrite by DL1BZ for public repeaters © <?php echo date("Y")." | served by ".$_SERVER['SERVER_SOFTWARE']." & PHP ".phpversion(); ?>
+<center><span title="Dashboard" style="font: 7pt arial, sans-serif;">based at <A HREF="https://github.com/FM-POLAND/hs_dashboard_pi">SvxLink Dashboard</A> by SP2ONG & SP0DZ | <A HREF="https://github.com/dl1bz/svxlinkdb4rptr">rewrite by DL1BZ</A> for public repeaters © <?php echo date("Y")." | served by ".$_SERVER['SERVER_SOFTWARE']." & PHP ".phpversion(); ?>
