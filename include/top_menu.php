21e3a57166a07e43f4d8d548982216098fdb747d
dl1bz <dg2dra@gmx.net>
2022-12-07 18:20:43 +0100

    change menu font

--- include/top_menu.php
+++ include/top_menu.php
@@ -1,7 +1,7 @@
 <p style="padding-right: 5px; text-align: right; color: #000000;"> 
-	<a href="/" style="color: #0000ff;">Dashboard</a> | 
-	<a href="/tg.php" style="color: #0000ff;">Talk Groups</a> | 
-	<a href="/node.php" style="color: #0000ff;">Nodes</a> |
+	<a href="/" style="color: #0000ff; font-family: 'Oswald', sans-serif; font-size: 14pt;">Dashboard</a> |
+	<a href="/tg.php" style="color: #0000ff; font-family: 'Oswald', sans-serif; font-size: 14pt;">Talk Groups</a> |
+	<a href="/node.php" style="color: #0000ff; font-family: 'Oswald', sans-serif;font-size: 14pt;">Nodes</a> |
 
 <?php
 $ip = isset($_SERVER['HTTP_CLIENT_IP'])?$_SERVER['HTTP_CLIENT_IP']:isset($_SERVER['HTTP_X_FORWARDED_FOR'])?$_SERVER['HTTP_X_FORWARDED_FOR']:$_SERVER['REMOTE_ADDR'];
@@ -11,10 +11,10 @@ $net3= cidr_match($ip,"127.0.0.0/8");
 $net4= cidr_match($ip,"10.0.0.0/8");
 
 if ($net1 == TRUE || $net2 == TRUE || $net3 == TRUE || $net4 == TRUE || FULLACCESS_OUTSIDE == 1) {
-	echo "<a href=\"/nodeInfo.php\" style=\"color: #0000ff;\"> NodeInfo</a> |";
-	echo "<a href=\"/dtmf.php\" style=\"color: #0000ff;\"> Dtmf</a> |";
-	echo "<a href=\"/log.php\" style=\"color: #0000ff;\"> Log</a> |" ;
-	echo "<a href=\"/power.php\" style=\"color: #0000ff;\"> Power</a>" ;
+	echo "<a href=\"/nodeInfo.php\" style=\"color: #0000ff; font-family: 'Oswald', sans-serif; font-size: 14pt;\"> NodeInfo</a> |";
+	echo "<a href=\"/dtmf.php\" style=\"color: #0000ff; font-family: 'Oswald', sans-serif; font-size: 14pt;\"> DTMF</a> |";
+	echo "<a href=\"/log.php\" style=\"color: #0000ff; font-family: 'Oswald', sans-serif; font-size: 14pt;\"> Log</a> |" ;
+	echo "<a href=\"/power.php\" style=\"color: #0000ff;font-family: 'Oswald', sans-serif; font-size: 14pt;\"> Power</a>" ;
 }
 ?>
 </p>
