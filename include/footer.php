a60ced19e9c6c2073bbb28b0bf346b294ad44cbd
dl1bz <dg2dra@gmx.net>
2023-04-14 10:28:39 +0200

    update footer

--- include/footer.php
+++ include/footer.php
@@ -1,2 +1,5 @@
 <!--- Please do not remove copyright info -->
-<center><span title="Dashboard" style="font: 7pt arial, sans-serif;">based at <A HREF="https://github.com/FM-POLAND/hs_dashboard_pi">SvxLink Dashboard</A> by SP2ONG & SP0DZ | <A HREF="https://github.com/dl1bz/svxlinkdb4rptr">rewrite by DL1BZ</A> for FM repeater & FM simplex systems © <?php echo date("Y")." | served by ".$_SERVER['SERVER_SOFTWARE']." & PHP ".phpversion(); ?>
+<!-- <center><span title="Dashboard" style="font: 7pt arial, sans-serif;">based at <A HREF="https://github.com/FM-POLAND/hs_dashboard_pi">SvxLink Dashboard</A> by SP2ONG & SP0DZ | <A HREF="https://github.com/dl1bz/svxlinkdb4rptr">rewrite by DL1BZ</A> for FM repeater & FM simplex systems © <?php echo date("Y")." | served by ".$_SERVER['SERVER_SOFTWARE']." & PHP ".phpversion(); ?> -->
+<center>
+<span title="Dashboard" style="font: 7pt arial, sans-serif;"><A HREF="https://github.com/dl1bz/svxlinkdb4rptr">SvxLink Dashboard by DL1BZ for FM repeater & FM simplex systems</A> © <?php echo date("Y")." | served by ".$_SERVER['SERVER_SOFTWARE']." & PHP ".phpversion(); ?>
+<br>based at <A HREF="https://github.com/FM-POLAND/hs_dashboard_pi">SvxLink Dashboard</A> by SP2ONG & SP0DZ
