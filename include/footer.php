ac4307b569d9c31a32c1e6501b94df95a518e8bd
dl1bz <dg2dra@gmx.net>
2023-04-14 10:40:55 +0200

    update footer

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,4 @@
 <!--- Please do not remove copyright info -->
 <center>
 <span title="Dashboard" style="font: 7pt arial, sans-serif;"><A HREF="https://github.com/dl1bz/svxlinkdb4rptr">SvxLink Dashboard by DL1BZ for FM repeater & FM simplex systems</A> © <?php echo date("Y")." | served by ".$_SERVER['SERVER_SOFTWARE']." & PHP ".phpversion(); ?>
-<br>parts of code based on project <A HREF="https://github.com/FM-POLAND/hs_dashboard_pi">SvxLink Dashboard by SP2ONG & SP0DZ</A>
+<br>parts of code were taken from project <A HREF="https://github.com/FM-POLAND/hs_dashboard_pi">SvxLink Dashboard by SP2ONG & SP0DZ</A>
